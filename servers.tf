3b7ea9a4b5040f72a2fad5675ce2f031f503a659
Hei Yiu <hyiubla@gmail.com>
2023-04-03 16:49:55 -0400

    add cloud init and bump server size

--- servers.tf
+++ servers.tf
@@ -1,9 +1,10 @@
 resource "hcloud_server" "my_cisandbox" {
   name         = "mycisandbox"
   image        = "ubuntu-22.04"
-  server_type  = "cpx11"
+  server_type  = "cpx21"
   location     = "ash"
   firewall_ids = [hcloud_firewall.server_firewall.id]
+  user_data    = file("./cloud-init.yaml")
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
