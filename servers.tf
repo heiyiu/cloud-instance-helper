dc0c91649ce39ac3d664d885a239dc3da8d6a923
Hei Yiu <hyiubla@gmail.com>
2023-04-17 18:21:01 -0400

    bump server size down during testing, first iteration of ansible playbook

--- servers.tf
+++ servers.tf
@@ -1,7 +1,9 @@
 resource "hcloud_server" "my_cisandbox" {
   name         = "mycisandbox"
   image        = "ubuntu-22.04"
-  server_type  = "cpx51"
+  # use cpx51 for ai image generation
+  # use cpx11 for regular cloud work
+  server_type  = "cpx11"
   location     = "ash"
   firewall_ids = [hcloud_firewall.server_firewall.id]
   user_data    = file("./cloud-init.yaml")
