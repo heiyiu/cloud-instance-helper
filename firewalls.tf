a67aadd622aec1e5dbdff8f4bdd6dfee51dd59e0
Hei Yiu <hyiubla@gmail.com>
2023-04-18 15:16:10 -0400

    fix firefall ports

--- firewalls.tf
+++ firewalls.tf
@@ -1,10 +1,38 @@
 # allow ssh connection from home ip address
 resource "hcloud_firewall" "server_firewall" {
   name = "server-firewall"
+  # ssh 
   rule {
     direction  = "in"
     protocol   = "tcp"
-    port       = "22,80,443,8443"
+    port       = "22"
+    source_ips = [
+      format("%s/32", var.home_ip)
+    ]
+  }
+  # http
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "80"
+    source_ips = [
+      format("%s/32", var.home_ip)
+    ]
+  }
+  # https
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "443"
+    source_ips = [
+      format("%s/32", var.home_ip)
+    ]
+  }
+  # https
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "8443"
     source_ips = [
       format("%s/32", var.home_ip)
     ]
