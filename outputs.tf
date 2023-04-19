15dd89bf25cdfc35f266a24d2d7f27ab32f10bf5
Hei Yiu <hyiubla@gmail.com>
2023-04-19 18:14:02 -0400

    output ipv4 address too

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@
 output "my_cisandbox_ipv6_address" {
   value = hcloud_server.my_cisandbox.ipv6_address
 }
+
+# ip of the created vm
+output "my_cisandbox_ipv4_address" {
+  value = hcloud_server.my_cisandbox.ipv4_address
+}
