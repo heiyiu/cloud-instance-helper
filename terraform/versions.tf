032b19e784955ff935edba21fa096bac93dc7320
Hei Yiu <hyiubla@gmail.com>
2023-04-26 17:24:46 -0400

    try switching to a local block

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,17 @@
 terraform {
+  # variable not supported in terraform block
+  locals {
+    workspace_tag = var.workspace_tag
+  }
   # use terraform cloud for backend
   # terraform cloud has development tag for the cloud-instance-helper project
   cloud {
     organization = "heicorp"
     hostname = "app.terraform.io"
+
+    workspaces {
+      tags = [ local.workspace_tag ]
+    }
   }
 }
 
-provider "terraform" {
-  workspaces {
-    tags = [var.workspace_tag]
-  }
-}
