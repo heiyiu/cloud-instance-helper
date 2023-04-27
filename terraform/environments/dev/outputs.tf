f406cc5439049901a96261bbde2e3f6fb5949894
Hei Yiu <hyiubla@gmail.com>
2023-04-27 09:28:48 -0400

    fix output name again

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,9 +1,9 @@
 # ip of the created vm
 output "sandbox_leader_server_ipv6_address" {
-  value = module.hertzner_cloud_stack.sandbox_leader_server.ipv6_address
+  value = module.hertzner_cloud_stack.hcloud_server.sandbox_leader_server.ipv6_address
 }
 
 # ip of the created vm
 output "sandbox_leader_server_ipv4_address" {
-  value = module.hertzner_cloud_stack.sandbox_leader_server.ipv4_address
+  value = module.hertzner_cloud_stack.hcloud_server.sandbox_leader_server.ipv4_address
 }
