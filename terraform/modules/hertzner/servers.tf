53c2f739c4a96e975106c862bef9081c5b4ec32e
Hei Yiu <hyiubla@gmail.com>
2023-06-22 19:46:43 -0400

    bump instance size

--- terraform/modules/hertzner/servers.tf
+++ terraform/modules/hertzner/servers.tf
@@ -5,7 +5,7 @@ resource "hcloud_server" "sandbox_leader_server" {
   image        = "67794396"
   # use cpx51 for ai image generation
   # use cpx11 for regular cloud work
-  server_type  = "cpx21"
+  server_type  = "cpx31"
   location     = "ash"
   firewall_ids = [hcloud_firewall.server_firewall.id]
   user_data    = file(format("%s/files/%s", path.module, var.cloud_init_filename))
